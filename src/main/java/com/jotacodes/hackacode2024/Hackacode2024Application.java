d38ea54f601d44faf3f869777104919b2c926450
JotaCodes <truendil@correo.com>
2025-01-22 15:57:08 +0100

    hackaton creation

---
+++ src/main/java/com/jotacodes/hackacode2024/Hackacode2024Application.java
@@ -0,0 +1,13 @@
+package com.jotacodes.hackacode2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackacode2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hackacode2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jotacodes/hackacode2024/Hackacode2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jotacodes.hackacode2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackacode2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
